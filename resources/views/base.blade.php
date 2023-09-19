5dcc9a11dab2479bc0fb094d6b499a6f05caf492
Mikołaj Cieśliczka <mikolajce@gmail.com>
2023-09-19 11:05:01 +0200

    19.09

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>Welcome!</title>
-        <link rel="stylesheet" href="/app.css">
+        <link rel="stylesheet" href="/public/app.css">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
